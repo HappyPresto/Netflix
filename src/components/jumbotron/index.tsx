204efb01046ed71200366d54715c89d81f3ad9c7
HappyPresto <darkstar54@mail.ru>
2021-02-05 18:59:05 +0300

    start , jumbotron, git, eslint

---
+++ src/components/jumbotron/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { ContainerS, Inner, Item, PaneS, TitleS, SubTitleS, ImageS } from './styles/jumbotron';
+
+interface IJumbotron {
+    direction: string,
+}
+
+interface IImage {
+    src: string,
+    alt: string
+}
+
+const Jumbotron: React.FC<IJumbotron> = ({children, ...restProps}) => {
+    return (
+        <Item {...restProps}>
+            <Inner>
+                {children}
+            </Inner>
+        </Item>
+    )
+}
+
+const Container: React.FC = ({children, ...restProps }) => {
+    return <ContainerS {...restProps}>{children}</ContainerS>
+}
+
+const Pane: React.FC = ({children, ...restProps}) => {
+    return <PaneS {...restProps}>{children}</PaneS>
+}
+
+const Title: React.FC = ({children, ...restProps}) => {
+    return <TitleS {...restProps}>{children}</TitleS>
+}
+
+const SubTitle: React.FC = ({children, ...restProps}) => {
+    return <SubTitleS {...restProps}>{children}</SubTitleS>
+}
+
+const Image: React.FC<IImage> = ({...restProps}) => {
+    return <ImageS {...restProps}/>
+}
+
+export default Object.assign(Jumbotron, {Container, Pane, Title, SubTitle, Image});
